fd1f7f300eb56bad9860751ac1cf8004a6f21cab
Chris Chow <chris.yo.chow@gmail.com>
2018-02-06 13:23:24 +0800

    Initial Commit

---
+++ DemoChatApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoChatApp
+//
+//  Created by Chris Chow on 6/2/2018.
+//  Copyright © 2018 Yau On Chow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
